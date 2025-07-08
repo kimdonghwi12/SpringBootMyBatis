f495a4a19cab828a3f3557feb1f64829cc81293e
kimdonghwi12 <tks12@naver.com>
2025-07-08 12:17:32 +0900

    hi

--- src/main/java/kopo/poly/controller/HomeController.java
+++ src/main/java/kopo/poly/controller/HomeController.java
@@ -1,4 +1,13 @@
 package kopo.poly.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";  // /WEB-INF/views/index.jsp
+    }
 }
